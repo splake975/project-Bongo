d20736be98258a941d911eafd40644cda63cd81d
splake975 <101668190+splake975@users.noreply.github.com>
2025-04-11 03:54:36 -0700

    test

---
+++ test.bat
@@ -0,0 +1,15 @@
+# @echo off
+:: Check if running as admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Change directory to the batch file's location
+cd /d "%~dp0"
+
+:: Your commands here
+echo Running in %cd%
+pause
